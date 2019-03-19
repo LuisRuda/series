2d802ee924a0f9fbb2546faaa04b6727a06cabf5
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-19 19:46:27 -0300

    setState email password

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -4,19 +4,40 @@ import { View, StyleSheet, TextInput } from 'react-native'
 import FormRow from '../components/FormRow'
 
 export default class LoginPage extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            mail: '',
+            password: ''
+        }
+    }
+
+    onChangeHandler(field, value) {
+        this.setState({ 
+            [field]: value
+         })
+    }
+
     render() {
         return (
             <View>
                 <FormRow>
                     <TextInput
                         style={styles.input}
-                        placeholder="user@email.com" />
+                        placeholder="user@email.com"
+                        value={this.state.mail}
+                        onChangeText={value => this.onChangeHandler('mail', value)}
+                    />
                 </FormRow>
                 <FormRow>
                     <TextInput
                         style={styles.input}
                         placeholder="******"
-                        secureTextEntry />
+                        secureTextEntry
+                        value={this.state.password}
+                        onChangeText={value => this.onChangeHandler('password', value)}
+                    />
                 </FormRow>
             </View>
         )
