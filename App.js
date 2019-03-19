868cf93a23eb5c10b86c4b95d3651e7a063af82e
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-19 18:26:34 -0300

    React Navigation

---
+++ App.js
@@ -0,0 +1,13 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+
+import LoginScreen from './src/pages/LoginScreen'
+
+const AppNavigator = createStackNavigator({
+    'Login': {
+        screen: LoginScreen
+    }
+})
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default AppContainer
