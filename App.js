e25fd8120b401dd2192fe0ffd15300f5413c4bcd
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-19 18:57:22 -0300

    Página de login

--- App.js
+++ App.js
@@ -4,7 +4,26 @@ import LoginScreen from './src/pages/LoginScreen'
 
 const AppNavigator = createStackNavigator({
     'Login': {
-        screen: LoginScreen
+        screen: LoginScreen,
+        navigationOptions: {
+            title: 'Bem vindo!'
+        }
+    }
+}, {
+    defaultNavigationOptions: {
+        title: 'Séries!',
+        headerTintColor: '#fff',
+        headerStyle: {
+            backgroundColor: '#6ca2f7',
+            borderBottomWidth: 1,
+            borderBottomColor: '#c5c5c5'
+        },
+        headerTitleStyle: {
+            color: '#fff',
+            fontSize: 30,
+            flexGrow: 1,
+            textAlign: 'center'
+        }
     }
 })
 
--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet, TextInput } from 'react-native'
+
+import FormRow from '../components/FormRow'
 
 export default class LoginPage extends React.Component {
     render() {
         return (
             <View>
-                <Text>Login</Text>
+                <FormRow>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="user@email.com" />
+                </FormRow>
+                <FormRow>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="******"
+                        secureTextEntry />
+                </FormRow>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    input: {
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingBottom: 5
+    }
+})
