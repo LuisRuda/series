6165516d41d4f2808e41cbb2fe51757c8a39223d
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-25 20:45:36 -0300

    Configurando o ReduxDevTools

---
+++ src/Router.js
@@ -0,0 +1,32 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+
+import LoginScreen from './pages/LoginScreen'
+
+const AppNavigator = createStackNavigator({
+    'Login': {
+        screen: LoginScreen,
+        navigationOptions: {
+            title: 'Bem vindo!'
+        }
+    }
+}, {
+    defaultNavigationOptions: {
+        title: 'Séries!',
+        headerTintColor: '#fff',
+        headerStyle: {
+            backgroundColor: '#6ca2f7',
+            borderBottomWidth: 1,
+            borderBottomColor: '#c5c5c5'
+        },
+        headerTitleStyle: {
+            color: '#fff',
+            fontSize: 30,
+            flexGrow: 1,
+            textAlign: 'center'
+        }
+    }
+})
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default AppContainer
