4e562cd1f76c9a6d1ea6db5b32629032cae42077
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-25 22:57:19 -0300

    Melhorando input de e-mail

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -107,6 +107,8 @@ class LoginPage extends React.Component {
                         placeholder="user@email.com"
                         value={this.state.mail}
                         onChangeText={value => this.onChangeHandler('mail', value)}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
                     />
                 </FormRow>
                 <FormRow last>
