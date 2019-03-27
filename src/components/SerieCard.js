f6b6398e1a6eddb69cd57372886c5a30601a4e78
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-26 22:10:53 -0300

    Estilizando title do component SerieCard

--- src/components/SerieCard.js
+++ src/components/SerieCard.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native'
 
 const SerieCard = ({ serie }) => (
     <View style={styles.container}>
         <View style={styles.card}>
-            <Text>{`${serie.id} - ${serie.title}`}</Text>
+            <Image
+                source={{
+                    uri: serie.img
+                }}
+                aspectRatio={1}
+                resizeMode="cover"
+            />
+            <View style={styles.cardTitleWrapper}>
+                <Text style={styles.cardTitle}>{serie.title}</Text>
+            </View>
         </View>
     </View>
 )
@@ -18,6 +27,24 @@ const styles = StyleSheet.create({
     card: {
         flex: 1,
         borderWidth: 1
+    },
+    cardTitleWrapper: {
+        backgroundColor: '#000',
+        height: 50,
+        position: 'absolute',
+        bottom: 0,
+        opacity: .8,
+        width: '100%',
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 3,
+        paddingRight: 3,
+        alignItems: 'center'
+    },
+    cardTitle: {
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold'
     }
 })
 
