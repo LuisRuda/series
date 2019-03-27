ad21080a8abac751cd7eaf223e7a61b038ec2b36
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-26 21:34:27 -0300

    Listando séries estaticas

--- src/pages/SeriesPage.js
+++ src/pages/SeriesPage.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, FlatList } from 'react-native'
 
-const SeriesPage = ({ series }) => (
+import series from '../../series.json'
+
+const SeriesPage = props => (
     <View>
-        <Text>Está é a SeriesPage</Text>
+        <FlatList
+            data={series}
+            renderItem={({ item }) => (
+                <View>
+                    <Text>{`${item.id} - ${item.title}`}</Text>
+                </View>
+            )}
+            keyExtractor={item => item.id}
+        />
     </View>
 )
 
