b10ff30dee0fc428ac0607cb237844a88815a789
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-04-01 18:47:36 -0300

    Exibindo os dados da série

--- src/pages/SerieDetailPage.js
+++ src/pages/SerieDetailPage.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { ScrollView, Text, StyleSheet, Image } from 'react-native'
 
-const SerieDetailPage = props => (
-    <View>
-        <Text>{props.navigation.state.params.serie.title}</Text>
-    </View>
-)
+import Line from '../components/Line'
+
+class SerieDetailPage extends React.Component {
+    render() {
+        return (
+            <ScrollView>
+                <Image 
+                    style={styles.image}
+                    source={{
+                        uri: this.props.navigation.state.params.serie.img
+                    }}
+                />
+                <Line label="Título" content={this.props.navigation.state.params.serie.title}/>
+                <Line label="Gênerp" content={this.props.navigation.state.params.serie.gender}/>
+                <Line label="Nota" content={this.props.navigation.state.params.serie.rate}/>
+                <Line label="Descrição" content={this.props.navigation.state.params.serie.description}/>
+            </ScrollView>
+        )
+    }
+}
 
 const styles = StyleSheet.create({
-
+    image: {
+        aspectRatio: 1
+    }
 })
 
 export default SerieDetailPage
