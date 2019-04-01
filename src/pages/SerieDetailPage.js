1cc26c6b693cd882fd38d6a56fea7b4a8c9546ab
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-04-01 19:43:44 -0300

    Collapse da descrição da série

--- src/pages/SerieDetailPage.js
+++ src/pages/SerieDetailPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { ScrollView, Text, StyleSheet, Image } from 'react-native'
 
 import Line from '../components/Line'
+import LongText from '../components/LongText'
 
 class SerieDetailPage extends React.Component {
     render() {
@@ -16,7 +17,7 @@ class SerieDetailPage extends React.Component {
                 <Line label="Título" content={this.props.navigation.state.params.serie.title}/>
                 <Line label="Gênerp" content={this.props.navigation.state.params.serie.gender}/>
                 <Line label="Nota" content={this.props.navigation.state.params.serie.rate}/>
-                <Line label="Descrição" content={this.props.navigation.state.params.serie.description}/>
+                <LongText label="Descrição" content={this.props.navigation.state.params.serie.description}/>
             </ScrollView>
         )
     }
