aa14b8d602f6e7e257c92ec8197faeb3135aa4c9
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-04-30 19:04:57 -0300

    Navegando para tela de edição de série

--- src/pages/SerieDetailPage.js
+++ src/pages/SerieDetailPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { ScrollView, Text, StyleSheet, Image } from 'react-native'
+import { ScrollView, Text, StyleSheet, Image, Button } from 'react-native'
 
 import Line from '../components/Line'
 import LongText from '../components/LongText'
 
 class SerieDetailPage extends React.Component {
     render() {
+        const { navigation } = this.props
+        const { serie } = navigation.state.params
         return (
             <ScrollView>
                 {
@@ -22,6 +24,10 @@ class SerieDetailPage extends React.Component {
                 <Line label="Gênerp" content={this.props.navigation.state.params.serie.gender} />
                 <Line label="Nota" content={this.props.navigation.state.params.serie.rate} />
                 <LongText label="Descrição" content={this.props.navigation.state.params.serie.description} />
+
+                <Button
+                    title="Editar"
+                    onPress={() => { navigation.navigate('SerieForm', { serieToEdit: serie }) }} />
             </ScrollView>
         )
     }
